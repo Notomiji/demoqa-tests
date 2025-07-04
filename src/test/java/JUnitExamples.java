f29b84a18cee771f0f116fe86fb5e99214c8b011
Notomiji <notomiji@gmail.com>
2025-07-04 15:14:12 +0300

    added tests

---
+++ src/test/java/JUnitExamples.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.*;
+
+public class JUnitExamples {
+
+    @BeforeAll
+    static void BeforeAll() {
+        System.out.println("This is BeforeAll");
+    }
+
+    @BeforeEach
+    void BeforeEach() {
+        System.out.println("    This is BeforeEach");
+    }
+
+    @AfterAll
+    static void AfterAll() {
+        System.out.println("This is AfterAll");
+    }
+
+    @AfterEach
+    void AfterEach() {
+        System.out.println("    This is AfterEach ");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("        This is firstTest");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("        This is secondTest");
+    }
+
+}
